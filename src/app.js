fb29d23d5fea814425da349b726bbe810a9b1030
derryman101 <derrynix@gmail.com>
2026-08-25 16:40:16 +0100

    feat: update application

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const html = `
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>AgroCD Demo</title>
+  <title>AgroCD Demo derry lomo </title>
   <style>
     * {
       box-sizing: border-box;
